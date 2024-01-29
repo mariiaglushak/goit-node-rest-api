1547ebe5abf9149cc4eca2b799130e81037df89c
mariiaglushak <90639229+mariiaglushak@users.noreply.github.com>
2024-01-29 12:30:39 +0200

    л

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -5,8 +5,6 @@ import { contactsModel } from "../shemas/contactsShemas.js";
 
 export const getAllContacts = async (req, res) => {
   const { id: owner } = req.user;
- 
-
   const contacts = await contactsModel.find({ owner });
   res.json(contacts);
 }
@@ -38,7 +36,8 @@ export const deleteContact = async(req,res,next)=>{
     if (removedContact === null) {
       next(HttpError(404,res[404]));
     };
-
+  
+    
     res.status(200).json({ message: "Contact deleted" });
     
   }catch{
@@ -51,7 +50,6 @@ export const deleteContact = async(req,res,next)=>{
 
 export const createContact = async (req, res, next) => {
   try {
-     
     const { id: owner } = req.user;
     const newContact= await contactsModel.create({...req.body, owner});
     res.status(201).json(newContact);
@@ -68,7 +66,6 @@ export const updateContact = async(req,res,next)=>{
 
   try{
     const { id } = req.params;
-  
     const updatedContacts = await contactsModel.findByIdAndUpdate(id, req.body, {new :true});
     if(updatedContacts === null){
       throw HttpError(404,res[404]);
