58a2de74a37cdc0c8b7526494699024aeafe9887
mariiaglushak <90639229+mariiaglushak@users.noreply.github.com>
2024-02-01 12:50:31 +0200

    Правки

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-import { config } from 'dotenv';
+import { config } from "dotenv";
 import "./db/db.js";
-import authRouter from "./routes/authRouter.js";
 import contactsRouter from "./routes/contactsRouter.js";
 import usersRouter from "./routes/usersRouter.js";
 
-
 config();
-
 
 const app = express();
 
@@ -17,11 +14,10 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRouter);
+app.use("/avatar", express.static("public/avatars"));
+
 app.use("/api/contacts", contactsRouter);
 app.use("/api/users", usersRouter);
-app.use("/avatars", express.static("public/avatars"));
-
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -3,13 +3,33 @@ import { uploadAvatar } from "../controllers/userControllers.js";
 import { authenticate } from "../middlewares/authenticate.js";
 import { upload } from "../middlewares/upload.js";
 import jimpProcessingAvatar from "../middlewares/jimpProcessing.js";
-
-
+import {
+  register,
+  login,
+  getCurrent,
+  logout,
+} from "../controllers/userControllers.js";
+import validateBody from "../helpers/validateBody.js";
+import { userJoiSchema } from "../shemas/usersShemas.js";
 
 const usersRouter = express.Router();
+const jsonParser = express.json();
 
-usersRouter.patch("/avatars", authenticate, upload.single("avatar"), jimpProcessingAvatar,  uploadAvatar);
-
+usersRouter.post(
+  "/register",
+  jsonParser,
+  validateBody(userJoiSchema),
+  register
+);
+usersRouter.post("/login", jsonParser, validateBody(userJoiSchema), login);
+usersRouter.get("/current", authenticate, getCurrent);
+usersRouter.get("/logout", authenticate, logout);
+usersRouter.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  jimpProcessingAvatar,
+  uploadAvatar
+);
 
 export default usersRouter;
-
